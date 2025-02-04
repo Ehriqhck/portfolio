0f330aa641680e1e8c21bcc26579fe6826a4a54e
Ehriqhck <eyang701@gmail.com>
2025-02-04 04:04:17 -0500

    before

--- components/fancy/PortfolioCard.jsx
+++ components/fancy/PortfolioCard.jsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { Transition } from "@headlessui/react";
 import UnderArrow from '@components/generic/Icons/UnderArrow.jsx'
 import CaseButton from '@components/fancy/CaseButton.jsx'
+
 import { BentoCard, BentoGrid } from "@/components/fancy/Bento.jsx";
 import { useMediaQuery } from 'usehooks-ts'
 import {
@@ -24,22 +25,6 @@ const DisplaySize = ({ width, height }) => (
     </h1>
   </div>
 );
-const Demo = React.forwardRef((props, ref) => {
-  const [width, setWidth] = useState(100);
-  const [height, setHeight] = useState(100);
-  console.log("child", ref.current);
-  useEffect(() => {
-    const resizeObserver = new ResizeObserver((event) => {
-      // Depending on the layout, you may need to swap inlineSize with blockSize
-      // https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserverEntry/contentBoxSize
-      setWidth(event[0].contentBoxSize[0].inlineSize);
-      setHeight(event[0].contentBoxSize[0].blockSize);
-    });
-
-    if (ref && ref.current) {
-      resizeObserver.observe(ref.current);
-    }
-  }, [ref]);
 const PortfolioCard = ({ CardContent, Section, Title, Slidercontent, Content: ContentTest, Figma }) => {
   const isMobile = useMediaQuery('(min-width: 640px)');
   const sliderData = Slidercontent;
